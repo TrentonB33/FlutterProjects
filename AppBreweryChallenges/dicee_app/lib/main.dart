295448693b3781c53bb221a3c9e1cb4a586757f8
BuildTools <unconfigured@null.spigotmc.org>
2019-09-28 22:57:06 -0600

    Updated Dicee app to be complete!

--- AppBreweryChallenges/dicee_app/lib/main.dart
+++ AppBreweryChallenges/dicee_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -19,28 +20,44 @@ class DicePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: pressed,
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                print('Right Button was tickled');
-              },
-              child: Image.asset('images/dice1.png'),
-            ),
-          ),
-        ],
-      ),
+      child: DicePane(),
     );
   }
 }
 
-void pressed() {
-  print('Button was pressed!');
+class DicePane extends StatefulWidget {
+  @override
+  _DicePaneState createState() => _DicePaneState();
+}
+
+class _DicePaneState extends State<DicePane> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: dicePressed,
+            child: Image.asset('images/dice$leftDiceNumber.png'),
+          ),
+        ),
+        Expanded(
+          child: FlatButton(
+            onPressed: dicePressed,
+            child: Image.asset('images/dice$rightDiceNumber.png'),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void dicePressed() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
 }
